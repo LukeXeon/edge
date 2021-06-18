9bc87b301b72fd196edf6fa07f65e09e970b6db7
发光的半导体 <694014820@qq.com>
2021-06-18 22:39:08 +0800

    m

--- edge/src/main/java/me/luke/edge/EdgeValue.kt
+++ edge/src/main/java/me/luke/edge/EdgeValue.kt
@@ -14,6 +14,7 @@ class EdgeValue(
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeLong(version)
+        parcel.writeParcelable(data, flags)
     }
 
     override fun describeContents(): Int {
